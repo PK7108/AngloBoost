f36a0d24e614a126c7e4c1161efd5a3190a9c332
PK7108 <patryk070108@interia.pl>
2025-11-22 16:34:13 +0100

    Improved LCP and FCP

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
-                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    vendor: ['react', 'react-dom'],      // Core React
+                    router: ['react-router-dom'],         // Routing osobno
+                    utils: ['lodash', 'axios'],           // Utilities osobno
                 }
             }
         }
